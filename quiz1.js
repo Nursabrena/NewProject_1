556aa6ff4689fc2920b23a12b5cd502d4d9be97f
nursabrena <nrsbrnaaaa11@gmail.com>
2023-03-30 16:22:48 +0800

    quiz1

---
+++ quiz1.js
@@ -0,0 +1,73 @@
+let message = "benr"       //variable
+console.log(message, typeof message)
+
+message = 123
+console.log(message, typeof message)
+
+//let str = "Benr"        //string
+//let num = 2533
+
+//let total = parseFloat(str) + num
+//console.log(total, typeof total)
+
+//let str = "Benr"        //Boolean
+//let num = false
+
+//let total = parseFloat(str) + num
+//console.log(total, typeof total)
+
+let str = "1.258"        //decimal
+let num = 1
+
+let total = parseFloat(str) + num
+console.log(total, typeof total)
+
+//let str = "1"        //int
+//let num = 1
+
+//let total = parseFloat(str) + num
+//console.log(total, typeof total)
+
+
+//start array 
+
+let data = ["benr",3533 ,true]
+
+console.log(data[0])
+console.log(data[1])
+console.log(data[2])
+
+
+//start object
+let user = {
+    name: "sab",
+    age: 20,
+    faculty: "FKEKK"
+}
+console.log(user.name)
+console.log(user.age)
+console.log(user.faculty)
+
+///////
+
+let messages = [
+{   name: "user1" ,
+    date: "1/1/22",
+    msg: "good video",
+    like: 1000,
+},
+{   name: "user2" ,
+    date: "1/2/22",
+    msg: "good sis",
+    like: 111,
+},
+{   name: "user3" ,
+    date: "2/2/22",
+    msg: "good job",
+    like: 111,
+},
+]
+console.log( messages[1].msg)  //good
+
+
+
